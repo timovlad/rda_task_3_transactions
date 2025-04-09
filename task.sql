a77d153fa26f35aee0479fe885f4860310b35306
timovlad <61474934+timovlad@users.noreply.github.com>
2025-04-09 17:18:06 +0200

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,12 +1,19 @@
-USE ShopDB;
+-- Use our database
+USE ShopDB; 
+
+-- Some data should be created outside the transaction (here)
+
 
 START TRANSACTION;
 
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, '2023-01-01');
+
 SET @OrderID = LAST_INSERT_ID();
+
 INSERT INTO OrderItems (OrderID, ProductID, Count)
 VALUES (@OrderID, 1, 1);
+
 UPDATE Products
 SET WarehouseAmount = WarehouseAmount - 1
 WHERE ID = 1;
