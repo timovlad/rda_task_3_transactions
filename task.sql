6621f5652f99284c991eff0c0f6e18da34c7a0d6
timovlad <61474934+timovlad@users.noreply.github.com>
2025-04-09 17:07:41 +0200

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,8 +1,8 @@
 USE ShopDB;
-INSERT INTO Customers (FirstName, LastName, Email, Address) VALUES ('John', 'Doe', 'j@dou.ua', 'Far, far away');
 
 START TRANSACTION;
 
+INSERT INTO Customers (FirstName, LastName, Email, Address) VALUES ('John', 'Doe', 'j@dou.ua', 'Far, far away');
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, '2023-01-01');
 SET @OrderID = LAST_INSERT_ID();
